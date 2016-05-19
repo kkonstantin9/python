5960fc02ab6ac3004321ab481796b229679184ec
fidel4ik <fidel4ik@gmail.com>
2016-05-20 00:53:28 +0300

    Create level04.lesson06.task04.java

---
+++ https/github.com/fidel4ik/level04.lesson04.task04package-com.javarush.test.level04.lesson04.task04/checkSeason.-.-2-5/level04.lesson06.task04.java
@@ -0,0 +1,22 @@
+package com.javarush.test.level04.lesson06.task04;
+
+/* Сравнить имена
+Ввести с клавиатуры два имени, и если имена одинаковые вывести сообщение «Имена идентичны». Если имена разные, но их длины равны – вывести сообщение – «Длины имен равны».
+*/
+
+import java.io.*;
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String na = reader.readLine();
+        String nb = reader.readLine();
+        if (na.equals(nb))
+            System.out.println("Имена идентичны");
+        else if (na.length()==nb.length())
+            System.out.println("Длины имен равны");
+
+    }
+}
